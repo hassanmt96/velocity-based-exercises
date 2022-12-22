004272ee6aa938b743cb7a6b6ea787ec491ceab1
Hassan Tariq <hassanmt996@gmail.com>
2022-12-21 23:43:24 -0600

    Set states for workout table

--- velocity-workout/src/components/Form.js
+++ velocity-workout/src/components/Form.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import WorkoutTable from "./WorkoutTable";
+import { Data } from "../Data";
+import { TextField, MenuItem } from "@mui/material";
 
-const Form = ({day}) => {
+const Form = ({ day }) => {
+  const [velocityRange, setVelocityRange] = useState("");
+  const [velocityName, setVelocityName] = useState("");
+
+  const changeVelocity = (e) => setVelocityRange(e.target.value);
+
+
+  const getVelocityName = () => {
+
+    //find matching velocity object
+    const currentVelocity = Data.Velocities.filter((velocity) =>{
+        return velocity.range == velocityRange;
+    });
+    // Statment needed to prevent property name "undefined"
+    if(currentVelocity[0]){
+        setVelocityName(currentVelocity[0].name);
+    }
+  }
+
+  useEffect(() => getVelocityName(), [velocityRange]);
+
   return (
-    <div>Form</div>
-  )
-}
+    <>
+      <div style={{ marginBottom: 20 }}>
+        <TextField
+          select
+          onChange={changeVelocity}
+          value={velocityRange}
+          helperText="Please select your max velocity from last phase."
+        >
+          {Data.Velocities.map((velocity) => {
+            return (
+              <MenuItem onChange={changeVelocity} value={velocity.range}>
+                {velocity.range}
+              </MenuItem>
+            );
+          })}
+        </TextField>
+      </div>
+      <div>
+        <WorkoutTable
+          day={day}
+          velocityRange={velocityRange}
+          velocityName={velocityName}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Form;
--- velocity-workout/src/components/WorkoutTable.js
+++ velocity-workout/src/components/WorkoutTable.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Data } from '../Data';
+import {
+    Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Paper, Container, Typography
+} from "@mui/material"
 
-const WorkoutTable = () => {
+const WorkoutTable = ({ day, velocityRange, velocityName }) => {
+
+    const [mainLift, setMainLift] = useState("");
+    const [exercises, setExercises] = useState([]);
+
+    const setUpperBody = () =>{
+        setMainLift("Bench Press");
+        setExercises(Data.UpperBody);
+    }
+
+    const setLowerBody = () => {
+        setMainLift("Squat");
+        setExercises(Data.LowerBody);
+    }
+
+
   return (
     <div>WorkoutTable</div>
   )
