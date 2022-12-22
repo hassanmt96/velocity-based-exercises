b3179089a1b4098c954d3d053516f0462ad04343
Hassan Tariq <hassanmt996@gmail.com>
2022-12-21 22:13:46 -0600

    Add Data File to render as SPA

---
+++ velocity-workout/src/Data.js
@@ -0,0 +1,31 @@
+const UpperBody = [
+    { name: "Overhead Press", setsReps: "3 X 6" },
+    { name: "Bent-Over BB Row", setsReps: "3 X 8" },
+    { name: "Pull-Ups", setsReps: "3 X 5-10" },
+    { name: "DB Bicep Curl", setsReps: "2 X 15-20" },
+    { name: "Banded Tricep Pushdown", setsReps: "2 X 15-20" },
+    { name: "Russian Twist", setsReps: "3 X 20ea" },
+  ];
+  
+  const LowerBody = [
+    { name: "RDL", setsReps: "3 X 6" },
+    { name: "Forward Lunge", setsReps: "3 X 8ea" },
+    { name: "Bulgarian SS", setsReps: "3 X 8ea" },
+    { name: "Nordic Hamstring Lowering", setsReps: "3 X 5" },
+    { name: "Barbell Hip Thrust", setsReps: "3 X 12" },
+    { name: "Stability Ball Plank", setsReps: "3 X 20-30s" },
+  ];
+  
+  const Velocities = [
+    { name: "Absolute Strength", range: "0.15 - 0.35" },
+    { name: "Accelerative Strength", range: "0.45 - 0.75" },
+    { name: "Strength-Speed", range: "0.75 - 1.0" },
+    { name: "Speed-Strength", range: "1.0 - 1.5" },
+    { name: "Starting Strength", range: ">1.5" },
+  ];
+  
+  export const Data = {
+    UpperBody,
+    LowerBody,
+    Velocities,
+  };
